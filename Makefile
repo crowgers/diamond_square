be4a0f0646e67e68b63276c6ebf65add8a4e94ce
crowgers <12558716+crowgers@users.noreply.github.com>
2021-11-09 20:38:17 +0000

    renamed entry point, made an algorithm directory (WIP) split plotting and algorithms steps up.

--- Makefile
+++ Makefile
@@ -3,18 +3,23 @@
 
 SHELL := /bin/bash
 
+# Run the project
 run:
-	python3 -m ds_main
+	python3 -m run
 
+# Minimal Environment Setup
 setup: _env_setup
 	pip3 install -r ${VIRTUAL_ENV}/../requirements/requirements.txt
 
+
+# Dev Environment Setup
 dev-setup: _env_setup
 	pip3 install -r ${VIRTUAL_ENV}/../requirements/dev-requirements.txt
 	pip3 install -r ${VIRTUAL_ENV}/../requirements/requirements.txt
 	pre-commit install --allow-missing-config; \
 	echo "INFO: Run 'source .venv/bin/activate' to complete local setup."; \
 
+# Creates a Virtual environment and upgrades pip
 _env_setup:
 	python3.9 -m venv .venv
 	source .venv/bin/activate; \
--- run.py
+++ run.py
@@ -5,16 +5,17 @@ numpy version 1.11.2
 matplotlib version 1.5.3
 requires mpl_toolkits.mplot3d library for 3d plots.
 """
-from diamond_square.methods import *
-
+from diamond_square.config import config
+from diamond_square.algorithm.methods import f_seed_grid, f_dsmain, f_plotting
 
 if __name__ == "__main__":
     # Inputs here. Recommend ds_steps = 3 up to 11 for 2d and 3 - 5 for 3d.
-    ds_steps = 5  # Number of levels. Grid points = ((ds_steps^2)+1)^2.
-    max_rnd = 1.0  # Min & Max random value.
-    plot_type = "3d"  # "3d" for 3d.  Makes 2d plot for any other input.
+    ds_steps = config.STEPS  # Number of levels. Grid points = ((ds_steps^2)+1)^2.
+    max_rnd = config.MAX_HEIGHT  # Min & Max random value.
+    plot_type = config.PLOT_TYPE  # "3d" for 3d.  Makes 2d plot for any other input.
     # Inputs end.
-    max_index = 2 ** ds_steps
+    max_index = config.GRID_SIZE
+
     seeded_map = f_seed_grid(2**ds_steps + 1)
     Final_height_map = f_dsmain(seeded_map, ds_steps, max_index, max_rnd)  # Calcs.
     f_plotting(Final_height_map, max_index, plot_type)  # Plotting.
